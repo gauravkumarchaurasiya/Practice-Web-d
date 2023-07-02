425f8690af8b001b58e87918400692d4b653746f
Gauravkumarchaurasiya <2002gauravkumarchaurasiya@gmail.com>
2023-07-02 22:25:49 +0530

    fix some bugs and add resume

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,17 +1,16 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    var scrollPosition = window.scrollY;
-  
-    if (scrollPosition > 10) {
-      navbar.classList.add('fixed');
-    } else {
-      navbar.classList.remove('fixed');
-    }
-  });
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector(".navbar");
+  var scrollPosition = window.scrollY;
 
+  if (scrollPosition > 10) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
 
 // Get all the card elements
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 
 // Function to handle scroll event
 const handleScroll = () => {
@@ -24,79 +23,67 @@ const handleScroll = () => {
 
     if (cardPosition < windowHeight * 0.99) {
       setTimeout(() => {
-        card.classList.add('animate');
+        card.classList.add("animate");
       }, delay);
       delay += 700; // Adjust the delay time between each card (in milliseconds)
     } else {
-      card.classList.remove('animate');
+      card.classList.remove("animate");
     }
   });
 };
 
 // Add scroll event listener
-window.addEventListener('scroll', handleScroll);
+window.addEventListener("scroll", handleScroll);
 
 // Trigger the animation on initial page load
 handleScroll();
 
+document.addEventListener("DOMContentLoaded", function () {
+  const themeToggle = document.querySelector(".theme-toggle");
+  const moonIcon = document.querySelector(".moon-icon");
+  const sunIcon = document.querySelector(".sun-icon");
+  const body = document.querySelector("body");
+  // console.log(sunIcon);
+  // console.log(sunIcon.classList);
+  // console.log("before");
+  sunIcon.classList.add("hidden");
+  // console.log(sunIcon);
+  // console.log(sunIcon.classList);
+  themeToggle.addEventListener("click", function () {
+    
+    body.classList.toggle("dark-theme");
+    
+    moonIcon.classList.toggle("hidden");
+    sunIcon.classList.toggle("shown");
+  });
+});
 
-  
+// Function to load and render the PDF document
+function loadPDF(url) {
+  // Fetch the PDF document
+  pdfjsLib.getDocument(url).promise.then(function(pdf) {
+    // Get the first page
+    pdf.getPage(1).then(function(page) {
+      var scale = 1.5;
+      var viewport = page.getViewport({ scale: scale });
 
+      // Create a canvas element to render the page
+      var canvas = document.createElement("canvas");
+      var context = canvas.getContext("2d");
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
+      document.getElementById("pdf-viewer").appendChild(canvas);
 
-// const themeToggle = document.querySelector('.theme-toggle');
-// const moonIcon = document.querySelector('.moon-icon');
-// const sunIcon = document.querySelector('.sun-icon');
-// const body = document.querySelector('body');
-// console.log(themeToggle);
-// themeToggle.addEventListener('click', toggleTheme);
+      // Render the page onto the canvas
+      page.render({
+        canvasContext: context,
+        viewport: viewport
+      });
+    });
+  });
+}
 
-// function toggleTheme() {
-//     console.log("clicked");
-//   body.classList.toggle('dark-theme');
-//   moonIcon.classList.toggle('hidden');
-//   sunIcon.classList.toggle('hidden');
-// }
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const themeToggle = document.querySelector('.theme-toggle');
-    const moonIcon = document.querySelector('.moon-icon');
-    const sunIcon = document.querySelector('.sun-icon');
-    const body = document.querySelector('body');
-  console.log("uinderf");
-  console.log(moonIcon);
-  console.log(sunIcon);
-  themeToggle.addEventListener('click', function () {
-    console.log("clicked");
-    console.log(body.classList);
-    body.classList.toggle('dark-theme');
-    console.log(body.classList);
-    moonIcon.classList.toggle('hidden');
-    console.log(body.classList);
-        sunIcon.classList.toggle('hidden');
-      });});
-
-
-//       const target = {
-//         clicked: 0,
-//         currentFollowers: 90,
-//         btn: document.querySelector("a.btn"),
-//         fw: document.querySelector("span.followers")
-//       };
-      
-//       const follow = () => {
-//         target.clicked += 1;
-//         target.btn.innerHTML = 'Following <i class="fas fa-user-times"></i>';
-      
-//         if (target.clicked % 2 === 0) {
-//           target.currentFollowers -= 1;
-//           target.btn.innerHTML = 'Follow <i class="fas fa-user-plus"></i>';
-//         }
-//         else {
-//           target.currentFollowers += 1;
-//         }
-      
-//         target.fw.textContent = target.currentFollowers;
-//         target.btn.classList.toggle("following");
-//       }
-      
+// Call the loadPDF function with the URL of your PDF file
+document.addEventListener("DOMContentLoaded", function() {
+  loadPDF("../Gkc resume.pdf");
+});
